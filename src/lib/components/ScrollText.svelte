e5cb97634a4422c80163fda036738b030cf42e52
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2025-01-23 13:06:58 -0800

    Sources

--- src/lib/components/ScrollText.svelte
+++ src/lib/components/ScrollText.svelte
@@ -32,15 +32,22 @@
     function fadeInOut(position: number, from: number, at: number, to: number) {
         return "opacity: "+clamp(Math.min(map(position, from, at, 0, 1), map(position, at, to, 1, 0)), 0, 1)+";";
     }
+
+    const sources = [
+        "https://www.zdnet.com/article/switzerland-now-requires-all-government-software-to-be-open-source/",
+        "https://ubuntu.com/blog/does-open-source-software-have-the-same-safety-as-proprietary-software",
+        "https://www.commerce.gov/about/policies/source-code",
+        "https://fedscoop.com/agencies-must-share-custom-source-code-under-new-share-it-act/"
+    ]
 </script>
 <svelte:document on:scroll={onScroll} />
 
 <div class="absolute w-full h-full text-white">
     <div class="relative h-screen flex flex-col justify-center items-center text-center">
         <div class="font-semibold flex flex-col items-center gap-4" style={fadeOut(position, 0.5, 0.75)}>
-            <div class="text-6xl"><TextGradient className="font-extrabold" from="#ff0f7b" to="#f89b29">Imagine</TextGradient> a world</div>
+            <div class="text-4xl sm:text-6xl"><TextGradient className="font-extrabold" from="#ff0f7b" to="#f89b29">Imagine</TextGradient> a world</div>
             <div class="text-2xl">where all <i>government code</i></div>
-            <div class="text-6xl">is <TextGradient className="font-extrabold" from="#84ffc9" via="#aab2ff" to="#eca0ff">Open Source</TextGradient>...</div>
+            <div class="text-4xl sm:text-6xl">is <TextGradient className="font-extrabold" from="#84ffc9" via="#aab2ff" to="#eca0ff">Open Source</TextGradient>...</div>
         </div>
         <div class="absolute bottom-16 text-center" style={fadeOut(position, 0.0, 0.5)}>
             <div class="float text-gray-500">▼ Scroll Down ▼</div>
@@ -49,35 +56,35 @@
 
 
     <div class="relative h-screen flex flex-col justify-center items-end text-end">
-        <div class="font-semibold flex flex-col items-center gap-4 pr-8" style={fadeOut(position, 1.0, 1.5)}>
-            <div class="text-4xl">...where you can <TextGradient className="font-extrabold text-6xl" from="#e9b7ce" to="#d3f3f1">Trust</TextGradient></div>
-            <div class="text-4xl">the voting machines...</div>
+        <div class="font-semibold flex flex-col items-center gap-4 pr-8" style={fadeInOut(position, 0.5, 1.0, 1.5)}>
+            <div class="text-2xl sm:text-4xl">...where you can <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#e9b7ce" to="#d3f3f1">Trust</TextGradient></div>
+            <div class="text-2xl sm:text-4xl">the voting machines...</div>
         </div>
     </div>
 
     <div class="relative h-screen"></div>
     <div class="fixed top-12 w-full flex flex-col items-center text-center" style={fadeInOut(position, 1.5, 2.0, 2.25)}>
-        <div class="text-4xl">...and have believe in the <TextGradient className="font-extrabold text-6xl" from="#ff1b6b" to="#45caff">Technology</TextGradient></div>
-        <div class="text-4xl">that <TextGradient className="font-extrabold text-6xl" from="#fbd07c" to="#f7f779">Powers</TextGradient> our union...</div>
+        <div class="text-2xl sm:text-4xl">...and believe in the <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#ff1b6b" to="#45caff">Technology</TextGradient></div>
+        <div class="text-2xl sm:text-4xl">that <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#fbd07c" to="#f7f779">Powers</TextGradient> our union...</div>
     </div>
 
     <div class="relative h-screen flex flex-col items-end text-end">
         <div class="font-semibold flex flex-col items-center gap-4 pt-24 pr-8" style={fadeInOut(position, 2.75, 3.0, 3.25)}>
-            <div class="text-4xl">...because the <TextGradient className="font-extrabold text-6xl" from="#82f4b1" to="#30c67c">Code</TextGradient></div>
-            <div class="text-4xl">is open to the <TextGradient className="font-extrabold text-6xl" from="#fffbaf" to="#ab2aed">People</TextGradient>...</div>
+            <div class="text-2xl sm:text-4xl">...because the <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#82f4b1" to="#30c67c">Code</TextGradient></div>
+            <div class="text-2xl sm:text-4xl">is open to the <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#fffbaf" to="#ab2aed">People</TextGradient>...</div>
         </div>
     </div>
 
     <div class="fixed top-12 w-full flex flex-col items-center text-center" style={fadeInOut(position, 1.5, 2.0, 2.25)}>
         
     </div>
-    <div class="relative min-h-screen flex flex-col items-center pt-12" style={fadeIn(position, 3.25, 4.0)}>
-        <div class="text-4xl font-semibold mb-12">
+    <div class="relative min-h-screen flex flex-col items-center pt-12 pb-24 px-4" style={fadeIn(position, 3.25, 4.0)}>
+        <div class="text-2xl sm:text-4xl font-semibold mb-12">
             to promote
         </div>
         <div class="w-full flex gap-8 flex-col items-center lg:flex-row lg:items-start lg:justify-evenly">
             <div class="flex flex-col items-center">
-                <div class="text-4xl text-center"><TextGradient className="font-extrabold text-6xl" from="#caefd7" via="#f5bfd7" to="#abc9e9">Transparency</TextGradient>,</div>
+                <div class="text-2xl sm:text-2xl text-center"><TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#caefd7" via="#f5bfd7" to="#abc9e9">Transparency</TextGradient>,</div>
                 <div class="indent-4 text-balance pt-8 max-w-sm">
                     <p class="mb-4">
                         Requiring all government code to be open source will allow the public to see how the government works.
@@ -90,7 +97,7 @@
                 </div>
             </div>
             <div class="flex flex-col items-center">
-                <div class="text-4xl text-center"><TextGradient className="font-extrabold text-6xl" from="#0061ff" to="#60efff">Security</TextGradient>,</div>
+                <div class="text-2xl sm:text-4xl text-center"><TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#0061ff" to="#60efff">Security</TextGradient>,</div>
                 <div class="indent-4 text-balance pt-8 max-w-sm">
                     <p class="mb-4">
                         The biggest thing is that <b>obscurity is not security</b>. The more people that can see the code, the more people that can find and fix vulnerabilities.
@@ -98,13 +105,13 @@
                         often more secure than closed source software because it is constantly being reviewed and improved by the community.
                     </p>
                     <p>
-                        Of course exceptions exist for sensitive information such as military secrets, personal data, or copyrighted material.
+                        Exceptions exist for sensitive information such as military secrets, personal data, or copyrighted material.
                     </p>
                 </div>
             </div>
             <div class="flex flex-col items-center">
-                <div class="text-4xl text-center whitespace-nowrap">and <TextGradient className="font-extrabold text-6xl" from="#82f4b1" to="#30c67c">Innovation</TextGradient></div>
-                <div class="indent-4 text-balance text-justify pt-8 max-w-sm">
+                <div class="text-2xl sm:text-4xl text-center whitespace-nowrap">and <TextGradient className="font-extrabold text-4xl sm:text-6xl" from="#82f4b1" to="#30c67c">Innovation</TextGradient></div>
+                <div class="indent-4 text-balance pt-8 max-w-sm">
                     <p>
                         This code was <b>paid for by our tax dollars</b>, and deserves to be available to the public to use and improve. Open source projects are often used as 
                         building blocks for other projects and valuable examples for developers. Having government code open source will allow for <b>more innovation</b> 
@@ -112,6 +119,32 @@
                     </p>
                 </div>
             </div>
+        </div>
+        <div class="w-3/4 my-16 h-[2px] bg-gray-600"></div>
+        <div class="text-4xl sm:text-6xl">
+            <TextGradient className="font-extrabold" from="#eca0ff" via="#aab2ff" to="#84ffc9">The Proposal</TextGradient>:
+        </div>
+        <div class="text-lg indent-4 text-balance mt-8 container text-center">
+            <p class="mb-4 font-bold">
+                We propose that 100% of custom developed software by government institutions, both federal and state, be open source, 
+                unless exempt for the reason of security, personal data, or prevented by other laws.
+            </p>
+            <p class="mb-4">
+                Additionally, this open source requirement extends to other contracted companies that develop software for vital government functions,
+                such as voting machines, and other critical infrastructure that could benefit from transparency. The specifics of this policy still need to be worked out.
+            </p>
+            <p class="mb-4">
+                This is an extension of the <a href="https://www.commerce.gov/about/policies/source-code" target="_blank" class="underline">U.S. Department of Commerce Source Code Policy</a>, which requires at least 20% of custom developed code to be open source.
+                We believe it should be 100% and extend to the private sector on certain areas for the reasons stated above.
+            </p>
+        </div>
+        <div class="w-3/4 my-16 h-[2px] bg-gray-600"></div>
+        <div class="text-sm container text-center text-gray-500">
+            <ul>
+                {#each sources as source}
+                    <li><a href={source} target="_blank" class="underline">{source}</a></li>
+                {/each}
+            </ul>
         </div>
     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,13 @@
 <ScrollText />
 
 <style>
+  :global(html) {
+    background: rgb(13, 19, 32);
+    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+  }
+
   :global(body) {
-    height: 500vh;
+    min-height: 500vh;
     margin: 0;
   }
 
